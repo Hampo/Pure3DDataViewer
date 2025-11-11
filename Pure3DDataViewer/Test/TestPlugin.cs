28d670f930c3fc682ccc03ffd7708547d47f2891
Proddy <josh@zhbot.org>
2025-11-11 01:17:10 +0000

    feat/Add `Test` plugin
    Used for testing various things without having to spin up a new plugin

---
+++ Pure3DDataViewer/Test/TestPlugin.cs
@@ -0,0 +1,26 @@
+﻿using Pure3DDataViewerPluginAPI.Interfaces;
+using System.Reflection;
+
+namespace Test;
+
+public class TestPlugin : IPlugin
+{
+    public string Name => "Test";
+
+    private static readonly List<IFileHandler> FileHandlers;
+
+    static TestPlugin()
+    {
+        FileHandlers = [
+            
+        ];
+
+        var assembly = Assembly.GetExecutingAssembly();
+    }
+
+    public IEnumerable<IFileHandler>? GetFileHandlers() => FileHandlers;
+
+    public IEnumerable<IChunkHandler>? GetChunkHandlers() => null;
+
+    public IEnumerable<IChunkEditor>? GetChunkEditors() => null;
+}
