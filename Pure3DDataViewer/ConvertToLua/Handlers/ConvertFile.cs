880155fb5b4422463c72806c82e2b55b1af8c109
Proddy <josh@zhbot.org>
2026-01-19 23:50:43 +0000

    feat/Added `ConvertFile` to `ConvertToLua`

--- Pure3DDataViewer/ConvertToLua/Handlers/ConvertFile.cs
+++ Pure3DDataViewer/ConvertToLua/Handlers/ConvertFile.cs
@@ -1,6 +1,8 @@
-﻿using NetP3DLib.P3D;
+﻿using ConvertToLua.Helpers;
+using NetP3DLib.P3D;
 using Pure3DDataViewerPluginAPI.Enums;
 using Pure3DDataViewerPluginAPI.Interfaces;
+using System.Text;
 
 namespace ConvertToLua.Handlers;
 public class ConvertFile : IFileHandler
@@ -16,6 +18,29 @@ public class ConvertFile : IFileHandler
 
     public FileCallbackResult Handle(P3DFile p3dFile)
     {
+        var sb = new StringBuilder();
+
+        sb.AppendLine("local P3DFile = P3D.P3DFile()");
+        sb.AppendLine();
+
+        foreach (var chunk in p3dFile.Chunks)
+        {
+            try
+            {
+                var constructor = ChunkMap.GetLuaConstructor(chunk);
+                sb.AppendLine($"P3DFile:AddChunk({constructor})");
+            }
+            catch (Exception ex)
+            {
+                sb.AppendLine($"-- Error in {chunk}: {ex.Message}");
+            }
+        }
+
+        sb.AppendLine();
+        sb.AppendLine("P3DFile:Output()");
+
+        Clipboard.SetText(sb.ToString());
+
         return FileCallbackResult.Unchanged;
     }
 
